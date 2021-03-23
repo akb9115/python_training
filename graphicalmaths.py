a9d1685c545249b68aa0f50edd07615f1ef052a8
Ajeet91990 <suman62091@gmail.com>
2021-03-23 12:42:44 +0530

    gh

---
+++ graphicalmaths.py
@@ -0,0 +1,22 @@
+import math
+           #area of circle and circumference#
+# radius = int(input("enter the radius of the circle"))
+# area = math.pi * radius * radius;
+# circ = 2 * math.pi * radius
+# print("Area of circle is ", area)
+# print("circference of circle",circ)
+           #area of square and perimeter#
+# side = int(input("enter of the square"))
+# area = side * side
+# perimeter = 4 * side
+# print("area of square is ",area)
+# print("area of perimeter is ",perimeter)
+
+               #area of rectangle
+length = float(input("enter the length of rectangle : "))
+breadth = int(input("enter the breadth of rectangle : "))
+area = length * breadth
+perimeter = 2 *(length + breadth)
+print("area of rectangle is",area)
+print("area of perimeter is ",perimeter)
+
